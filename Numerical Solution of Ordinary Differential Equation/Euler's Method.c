f79daf5c7fb5003b3450dde32b53062d93c5890b
Dipraj Debnath <42785078+DiprajDebnath@users.noreply.github.com>
2019-04-24 20:57:15 +0530

    Create Euler's Method.c

---
+++ Numerical Solution of Ordinary Differential Equation/Euler's Method.c
@@ -0,0 +1,43 @@
+/*
+  Eulers Method
+*/
+#include<stdio.h>
+#include<math.h>
+
+//function of dy/dx
+float f(float x, float y)
+{
+    return (x+y);
+}
+
+main()
+{
+    int i;
+    float x0, x1, h, y1, y0, xn;
+    /*
+     h = step size
+     y(x0)=y0, where x0 and y0 are initial values
+    */
+    printf("Enter the value of x0 and y0.\n");
+    scanf("%f %f", &x0, &y0);
+    
+    printf("Enter the value of step length h :");
+    scanf("%f",&h);
+    
+    printf("Enter the value of Xn for which y is required.\n");
+    scanf("%f",&xn);
+    
+    printf("When x%d=%f\ty%d=%f \n",i,x0,i,y0);
+    i++;
+
+    while(x0 < xn)
+    {
+        y1 = y0 + h * f(x0, y0);
+        x1 = x0 + h;
+        x0 = x1;
+        y0 = y1;
+        printf("When x%d=%f\ty%d=%f \n",i,x0,i,y0);
+        i++;
+    }
+}
+    
